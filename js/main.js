f3c7c1a817899889dbb7622757deee3fa9dd737e
revalew <115182776+revalew@users.noreply.github.com>
2023-02-21 20:02:33 +0100

    created header and nav section

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active')
+	navMobile.classList.toggle('nav-mobile--active')
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+handleCurrentYear()
+navBtn.addEventListener('click', handleNav)
